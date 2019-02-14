ce7882504e2f8a92884e807a6ff8e3b637d7470a
Marouane Souah <marouanesouah@gmail.com>
2019-02-14 23:18:40 +0100

    Add Printer provider

---
+++ src/providers/printer/printer.ts
@@ -0,0 +1,140 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
+import { AlertController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
+/*
+  Generated class for the PrinterProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class PrinterProvider {
+
+
+  selectedPrinter = null;
+  articles = [];
+  paiement = [];
+  order_id = null;
+
+
+  constructor(public http: HttpClient,private btSerial:BluetoothSerial,private nativeStorage: Storage,private alertCtrl:AlertController) {
+  }
+
+
+  savePrinter() {
+    this.nativeStorage.set("selectedPrinter",this.selectedPrinter);
+  }
+  searchBt()
+  {
+    return this.btSerial.list();
+  }
+
+
+
+  write() {
+
+  //  let client_name= "Client : Marouane".padEnd(24, " ");
+  // let chauffaur_name = "Chauffeur : Mohammed".padStart(24, " ");
+  // let quartier = "Quartier : Elhoda".padEnd(24, " ");
+  // let date = new Date().toLocaleString().padStart(24, " ");
+
+  let articles = JSON.parse('[{"designation":"valencia 1l","prix_vente":12,"quantite":3},{"designation":"valencia 1l","prix_vente":12,"quantite":2}]');
+
+  let articles_list = "";
+  articles.forEach(e => {
+    articles_list += e.designation.padEnd(15, " ")  + e.quantite.toString().padStart(3, " ").padEnd(6, " ") + (e.prix_vente + "DH").toString().padStart(4, " ").padEnd(8, " ") + ((e.quantite * e.prix_vente) + "DH").toString().padStart(5, " ").padEnd(12, " ") + "0"+`\r`;
+  });
+  let client_name= "Marouane souah";
+  let chauffaur_name = "Mohammed Belo";
+  let quartier_name = "Quartier : Elhoda".padEnd(24, " ");
+  let date = new Date().toLocaleString().padStart(24, " ");
+  let total_net = "Total NET a payer : 60 DH".padStart(45, " ");
+  let type_payment = "Type payment : Cheque".padStart(45, " ");
+let printData =
+`
+-----------------------------------------------\r
+                 Sté DISTRILAY \r
+           AV de la meque, Bd mohamed 6.\r
+         Manolo N° 345 BP:52, LAAYOUN 70020 \r
+           Tél: 0528991322 / 0661283916\r
+----------------------------------------------\r
+${"Client".padEnd(24, " ")}${"Chauffeur".padStart(18, " ")}\r
+${client_name.padEnd(24, " ")}${chauffaur_name.padStart(22, " ")}\r
+${quartier_name}${date}\r\r
+----------------------------------------------\r
+                  FACTURE N ${this.order_id} \r
+----------------------------------------------\r
+Article       | Qte | Prix | Total | Promotion \r
+----------------------------------------------\r
+${articles_list}
+----------------------------------------------\r
+${total_net}\r
+${type_payment}\r
+\r      
+\r
+`;
+
+    return new Promise((resolve,reject)=> {
+      this.btSerial.write(printData).then(dataz=>{
+          console.log("WRITE SUCCESS",dataz);
+    
+          let mno=this.alertCtrl.create({
+            title:"Impression SUCCESS!",
+            buttons:['Dismiss']
+          });
+          mno.present();
+          
+          // xyz.unsubscribe();
+          resolve(true);
+        },errx=>{
+          console.log("WRITE FAILED",errx);
+          let mno=this.alertCtrl.create({
+            title:"ERROR "+errx,
+            buttons:['Dismiss']
+          });
+          mno.present();
+          resolve(true);
+        });
+    });
+  }
+
+  print()
+  {
+  
+  
+
+    return new Promise((resolve,reject)=> {
+
+        this.btSerial.isConnected().then((value)=> {
+            this.write().then(()=> {
+              resolve(true);
+            }).catch(()=> {
+              resolve(true);
+            })
+        }).catch(()=> {
+          let xyz=this.btSerial.connect(this.selectedPrinter.address).subscribe(data=>{
+            this.write().then(()=> {
+              resolve(true);
+            }).catch(()=> {
+              resolve(true);
+            })
+          },err=>{
+            console.log("CONNECTION ERROR",err);
+            let mno=this.alertCtrl.create({
+              title:"ERROR "+err,
+              buttons:['Dismiss']
+            });
+            mno.present();
+            resolve(true);
+          });
+        })
+        
+    });
+   
+
+  }
+
+}
